d8ddc18a5bfe6f2538f294acd78cfe33da8206d4
pleonova <p.leonova@gmail.com>
2018-08-25 19:49:39 -0700

    Clean up code and add more notes

--- TimeKeeping/extract_google_calendar_events.py
+++ TimeKeeping/extract_google_calendar_events.py
@@ -24,9 +24,6 @@ from oauth2client import file, client, tools
 import pandas as pd
 import numpy as np
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-
 
 # Set up key and access file/folder
 mason_jar_path = 'C:/Users/Leonova/Dropbox/Time Keeping - Mason Jar/'
@@ -35,6 +32,9 @@ credential_file_name = 'credentials_client_secret_google_calendar.json'
 
 CLIENT_SECRET_FILE = mason_jar_path + credential_file_name
 APPLICATION_NAME = 'Mason Jar Calendar'
+TOKEN = mason_jar_path + 'token.json'
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 
 
 
@@ -42,7 +42,7 @@ def main():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    store = file.Storage(mason_jar_path + 'token.json')
+    store = file.Storage(TOKEN)
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
@@ -53,10 +53,16 @@ def main():
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     
-    # Details about the events().list() class
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
+    # Details about the events().list() class: 
+    # https://developers.google.com/calendar/v3/reference/events/list
+    events_result = service.events().list(
+            calendarId='primary', 
+            timeMin=now,
+            maxResults=10, 
+            singleEvents=True,
+            orderBy='startTime').execute()
+    
+    # We want to extract the things that are stored in items
     events = events_result.get('items', [])
 
     if not events:
@@ -64,33 +70,15 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
 
+    # Sometimes description is omitted entirely
         if 'description' in event:
             desc = event['description']
         else:
             desc = 'no description exists'
-                    
+      
+    # We want our final out put to have the start, name of event and description              
         print(start, event['summary'], desc)
-        
-    #### try to add description, but it only appears sometimes   
 
-        if 'description' in event:
-            return(event['description'])
-        else:
-            return('no description exists')
-    
-        try:
-           a = events[0]['description'] 
-        except:
-            end = 'error'
-   
-df = pd.DataFrame(np.array(events[0])) #.reshape(3,3), columns = list("abc"))
-     
-#        
-#    for description in descriptions:
-#        des = description['description']
-#        print(des)
-#
 
-######
 if __name__ == '__main__':
     main()
